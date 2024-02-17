ad4b2009d7db40374828a850f94f3d255305c8b5
ramlohith <ramlohith@MSI>
2024-02-17 18:02:38 +0530

    1.)  Added CRUD operation for CategoriesController 2.) Added Application dbContext 3.) Add migration for code first approact to the project 4.) Added DTOs for the Model folder to fetct/retireve/operations for the Category Controller 5.) Added Repositories 6.) configured using Db connection string appsettings.json

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,140 @@
+﻿using CodePlus.API.Models.Domain;
+using CodePlus.API.Models.DTO;
+using CodePlus.API.Repositories.Implementation;
+using CodePlus.API.Repositories.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePlus.API.Controllers
+{
+    // https://localhost:xxxx/api/categories
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryRepository _categoryRepository;
+
+        public CategoriesController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+
+        [HttpPost]
+        //[Authorize(Roles = "Writer")]
+        public async Task<IActionResult> CreateCategory([FromBody] CreateCategoryRequestDto request)
+        {
+            // Map DTO to Domain Model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await _categoryRepository.CreateAsync(category);
+
+            // Domain model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
+
+        // GET: https://localhost:7226/api/Categories
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+
+            //Map Domain model to DTO
+
+            var response = new List<CategoryDto>();
+            foreach (var category in categories)
+            {
+                response.Add(new CategoryDto { Id = category.Id, Name = category.Name, UrlHandle = category.UrlHandle });
+            }
+            return Ok(response);
+        }
+
+        // GET: https://localhost:7226/api/categories/{id}
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> GetCategoryById([FromRoute] Guid id)
+        {
+            var existingCategory = await _categoryRepository.GetById(id);
+            if(existingCategory != null)
+            {
+                return NotFound();
+            }
+
+            var response = new CategoryDto
+            {
+                Id = existingCategory.Id,
+                Name = existingCategory.Name,
+                UrlHandle = existingCategory.UrlHandle
+            };
+
+            return Ok(response);
+        }
+
+        // PUT: https://localhost:7226/api/categories/{id}
+        [HttpPut]
+        [Route("{id:Guid}")]
+        //[Authorize(Roles = "Writer")]
+        public async Task<IActionResult> EditCategory([FromRoute] Guid id, UpdateCategoryRequestDto request)
+        {
+            // Convert DTO to Domain Model
+            var category = new Category
+            {
+                Id = id,
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            category = await _categoryRepository.UpdateAsync(category);
+
+            if(category != null)
+            {
+                return NotFound();
+            }
+
+            //convert Domain model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+            return Ok(response);
+        }
+
+        // DELETE: https://localhost:7226/api/categories/{id}
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        //[Authorize(Roles = "Writer")]
+        public async Task<IActionResult> DeleteCategory([FromRoute] Guid id)
+        {
+            var category = await _categoryRepository.DeleteAsync(id);
+
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+            // Convert Domain model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+        }
+
+    }
+}
---
+++ Repositories/Implementation/CategoryRepository.cs
@@ -0,0 +1,69 @@
+﻿using CodePlus.API.Data;
+using CodePlus.API.Models.Domain;
+using CodePlus.API.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodePlus.API.Repositories.Implementation
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CategoryRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            
+        }
+        public async Task<Category> CreateAsync(Category category)
+        {
+            if (category != null)
+            {
+                await _dbContext.Categories.AddAsync(category);
+                await _dbContext.SaveChangesAsync();
+            }
+            else
+            {
+                category = null;
+            }
+            return category;
+        }
+
+        public async Task<Category?> DeleteAsync(Guid id)
+        {
+            var existingCategory = await _dbContext.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (existingCategory is null)
+            {
+                return null;
+            }
+
+            _dbContext.Categories.Remove(existingCategory);
+            await _dbContext.SaveChangesAsync();
+            return existingCategory;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _dbContext.Categories.ToListAsync();
+        }
+
+        public async Task<Category?> GetById(Guid id)
+        {
+            return await _dbContext.Categories.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Category?> UpdateAsync(Category category)
+        {
+            var existingCategotory = await _dbContext.Categories.FirstOrDefaultAsync(x => x.Id == category.Id);
+
+            if(existingCategotory !=  null)
+            { 
+                _dbContext.Entry(existingCategotory).CurrentValues.SetValues(category);
+                await _dbContext.SaveChangesAsync();
+                return category;
+            }
+
+            return null;
+        }
+    }
+}
