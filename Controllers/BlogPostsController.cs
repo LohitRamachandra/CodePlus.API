7a6f84adccc165ced844cba819c90219d713d867
ramlohith <ramlohith@MSI>
2024-03-23 22:00:55 +0530

    Done with Add New Blog Post API

---
+++ Controllers/BlogPostsController.cs
@@ -0,0 +1,76 @@
+﻿using CodePlus.API.Models.Domain;
+using CodePlus.API.Models.DTO;
+using CodePlus.API.Repositories.Implementation;
+using CodePlus.API.Repositories.Interface;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePlus.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogPostsController : ControllerBase
+    {
+        private readonly IBlogPostRepository _blogPostRepository;
+        public BlogPostsController(IBlogPostRepository blogPostRepository)
+        {
+            _blogPostRepository = blogPostRepository;
+        }
+
+        // POST: {apibaseurl}/api/blogposts
+        [HttpPost]
+        //[Authorize(Roles = "Writer")]
+        public async Task<IActionResult> CreateBlogPost([FromBody] CreateBlogPostRequestDto request)
+        {
+            // Convert DTO to DOmain
+            var blogPost = new BlogPost
+            {
+                Author = request.Author,
+                Content = request.Content,
+                FeaturedImageUrl = request.FeaturedImageUrl,
+                IsVisible = request.IsVisible,
+                PublishedDate = request.PublishedDate,
+                ShortDescription = request.ShortDescription,
+                Title = request.Title,
+                UrlHandle = request.UrlHandle
+                //Categories = new List<Category>()
+            };
+
+
+            //foreach (var categoryGuid in request.Categories)
+            //{
+            //    var existingCategory = await categoryRepository.GetById(categoryGuid);
+            //    if (existingCategory is not null)
+            //    {
+            //        blogPost.Categories.Add(existingCategory);
+            //    }
+            //}
+
+            blogPost = await _blogPostRepository.CreateAsync(blogPost);
+
+            // Convert Domain Model back to DTO
+            var response = new BlogPostDto
+            {
+                Id = blogPost.Id,
+                Author = blogPost.Author,
+                Content = blogPost.Content,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                IsVisible = blogPost.IsVisible,
+                PublishedDate = blogPost.PublishedDate,
+                ShortDescription = blogPost.ShortDescription,
+                Title = blogPost.Title,
+                UrlHandle = blogPost.UrlHandle,
+                //Categories = blogPost.Categories.Select(x => new CategoryDto
+                //{
+                //    Id = x.Id,
+                //    Name = x.Name,
+                //    UrlHandle = x.UrlHandle
+                //}).ToList()
+            };
+
+            return Ok(response);
+        }
+
+    }
+}
---
+++ Repositories/Implementation/BlogPostRepository.cs
@@ -0,0 +1,24 @@
+﻿using CodePlus.API.Data;
+using CodePlus.API.Models.Domain;
+using CodePlus.API.Repositories.Interface;
+
+namespace CodePlus.API.Repositories.Implementation
+{
+    public class BlogPostRepository : IBlogPostRepository
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public BlogPostRepository(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        public async Task<BlogPost> CreateAsync(BlogPost blogPost)
+        {
+            await dbContext.BlogPosts.AddAsync(blogPost);
+            await dbContext.SaveChangesAsync();
+            return blogPost;
+        }
+    }
+}
